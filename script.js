7d2674184d5cb60f318efaafdd899594dd2e65cf
Mahmadul Islam <mahmadul674@gmail.com>
2025-05-04 20:23:07 +0600

    Clock 1.0.1

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-const hrs = document.querySelector("#hrs")
-const min = document.querySelector("#min")
-const sec = document.querySelector("#sec")
-
+const hrs = document.querySelector("#hrs");
+const min = document.querySelector("#min");
+const sec = document.querySelector("#sec");
+const ampm = document.querySelector("#ampm"); // Add this in your HTML
 
 setInterval(() => {
   let currentTime = new Date();
-  hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours()
-min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes()
-sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds()
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  let seconds = currentTime.getSeconds();
+  let session = "AM";
 
-}, 1000);
+  if (hours >= 12) {
+    session = "PM";
+  }
+
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+
+  hrs.innerHTML = (hours < 10 ? "0" : "") + hours;
+  min.innerHTML = (minutes < 10 ? "0" : "") + minutes;
+  sec.innerHTML = (seconds < 10 ? "0" : "") + seconds;
+  ampm.innerHTML = session;
+
+}, 1000); // 1000ms = 1s is enough for a clock
 
 
-// console.log(currentTime.getHours());
 
+// const hrs = document.querySelector("#hrs")
+// const min = document.querySelector("#min")
+// const sec = document.querySelector("#sec")
+
+
+// setInterval(() => {
+//   let currentTime = new Date();
+//   hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours()
+//   min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes()
+//   sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds()
+
+// }, 10);
 
